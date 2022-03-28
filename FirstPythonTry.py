a36f2363ccc8432cfb60a4a580caa98198af3504
NamigRustamov <namigrustam@gitub.com>
2022-03-28 13:44:44 +0400

    commit

--- FirstPythonTry.py
+++ FirstPythonTry.py
@@ -18,6 +18,34 @@ mueyyenlesdirir.
 10- Pythonda olan data tiplərindən hansıları Javascript və C#da da var? Js ile number, str, boolen, list-array. C# ile int, float, dool,str.
 11- Sizcə hər dildə fərqli sayda data tiplərinin olmasının səbəbi nədir? fərqli novde məlumatlarla işləmək üçün.
 
-
-
 """
+
+
+#Day05.
+"""
+Pythonda neçə tip operator var? 
+Python operatorlari asagidaki 7 gruplara ayrilir:
+
+Arithmetic operators- matematik emeliyyatlar ucun
+Assignment operators- deyerleri deyiskenlere tanitmaq ucun
+Comparison operators- deyerleri muqayese etmek ucun
+Logical operators- sherti statementlari kombine yazmaq ucun(and, or, if)
+Identity operators- yaddashda olan eyni obyektleri mueyyen edir
+Membership operators- obyekt daxilinde ardicilligi mueyyen edir
+Bitwise operators- binar reqemleri muqayese edir"""
+
+"""Python + operatoru fərqli data tipləri ilə necə işləyir? reqemleri(int) toplayir, sozleri(str) birlesdirir.
+Type Conversion nədir və niyə ehtiyac duyuruq. Bir data type basqasina cevirir. 
+Type Conversion 2 nov olur: Implicit Type Conversion - avtomatik datanin cevrilmesi ve Explicit Type Conversion - istifadecinin telebi uzere
+cevirme. 
+Müqaisə operatorları və şərtlər arasında əlaqə necə qurulur? 2 deyerler arasinda muqayese etmek yoluyla, yani:
+x == y: burada x y deyerine bereberdimi?
+x != y: burada x y deyerine bereber olmadigini gostermek ucun
+x > y veya x < y : burada x y deyerinden boyuk veya kicik olmasini gosterer
+x >= y veya x <= y: burada x y deyerinden boyukya da beraber veya kicik ya da beraber olmasini gosterer
+
+
+indentation nədir? Python sintaksisində əhəmiyyəti nədir? Yazilan melumatin onunde boslugun buraxilmasidir.
+Python indentation Python interpretera mueyyen grup melumatin mueyyen block of code-a aid olmasini gosterir.
+"""
+
