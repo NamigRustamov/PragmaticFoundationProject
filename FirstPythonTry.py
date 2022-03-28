1a1934a085e1a2b6424c7fb08420aacc6ffe2b95
NamigRustamov <namigrustam@gitub.com>
2022-03-28 13:34:39 +0400

    commit

--- FirstPythonTry.py
+++ FirstPythonTry.py
@@ -1,1 +1,23 @@
-x=5
+#Day03-Day04
+"""Suallar:
+1- Source code deyəndə nə nəzərdə tutulur? Source code insan terefinden her hansi program dilinde yazilan koddur.  
+Translator bu kodu machine dilinde koda cevirir.
+2- Source code və machine code arasında fərq nədir? Source code insan yazir ki computer machine coda cevirib programcinin emrlerini icra etsin. 
+Machine code or machine language  instructionlardi ki,  birbasa  computerin  prosessoru terefinden icra olunur.
+3- Niyə bizim yazdığımız kodlar tərcümə edilir? Cunku computer birbasha bizim emrleri anlamir. 
+4- İnterpreter proqram necə  işləyir? İnterpreter proqram yazilan emrleri birbasha icra edir
+5- Compiler program necə işləyir? Compiler program once yazilan codu high-level programming language-dan lower level language cevirir ki 
+icra olunabilsin.
+6- Dəyişənlər proqramlaşdırma dilində niyə istifadə olunur? Dəyişənlər proqramlaşdırma dilində istifade olunan deyerleri saxlanmasinda 
+istifade olunur.
+7- Dəyişən təyin edərkən hansı formada ad verilə bilər? Kicik ve ya boyuk herfle baslayabiler ve ancaq herf, regem ve alt xett icerebiler, 
+ama reqemle baslayabilmez. Boyuk, kicik veya tam boyuk/tam kicik olmasi ferqli deyiskenler olacaq, buna da case sensitive deyilir.
+8- Mən Azərbaycan əlifbası ilə dəyişən təyin edə bilərəmmi? Beli.
+9- Biz niyə dataları tiplərə bölürük? Cunku deyiskenlere verilen deyerler ferqli ola biler. Ve ferqli data tipleriyle gorulen emeliyyatlari 
+mueyyenlesdirir.
+10- Pythonda olan data tiplərindən hansıları Javascript və C#da da var? Js ile number, str, boolen, list-array. C# ile int, float, dool,str.
+11- Sizcə hər dildə fərqli sayda data tiplərinin olmasının səbəbi nədir? fərqli novde məlumatlarla işləmək üçün.
+
+
+
+"""
